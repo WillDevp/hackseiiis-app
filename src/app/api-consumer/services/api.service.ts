b7ee289c135705ec400cbe7bf0d33d5a80fcc947
WillDev <wilmar.garciava@gmail.com>
2023-10-20 10:44:39 -0500

    ✨ feat(api-service): implemented dynamic API service

--- src/app/api-consumer/services/api.service.ts
+++ src/app/api-consumer/services/api.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ConfigService } from './config/config.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+private apisConfig: any;
+  constructor(private http: HttpClient, private configService: ConfigService) {
+    this.configService.getConfig().subscribe((data) => {
+      this.apisConfig = data;
+    });
+   }
+   getApiData(apiName: string): Observable<any>{
+    const apiConfig = this.apisConfig.find((api: any) => api.name === apiName);
+    return this.http.get(apiConfig.endPoint);
+   }
 }
